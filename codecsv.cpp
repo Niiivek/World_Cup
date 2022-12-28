81993bebc47e7c5d11b9d2225246fd0027fc859e
viitality <benmomo2001@gmail.com>
2022-12-28 14:57:12 +0100

    code CSV

---
+++ codecsv.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+ 
+using namespace std;
+
+int main()
+{
+    string fname="team_q.csv";
+    
+    vector<vector<string> > content;
+    vector<string> row;
+    string line, word;
+ 
+    fstream file (fname, ios::in);
+    if(file.is_open())
+    {
+        while(getline(file, line))
+        {
+            row.clear();
+ 
+            stringstream str(line);
+ 
+            while(getline(str, word, ','))
+            row.push_back(word);
+            content.push_back(row);
+            
+        }
+    }
+    else
+    cout<<"fichier pas ouvert \n";
+ 
+    for(int i=0;i<content.size();i++)
+    {
+        for(int j=0;j<content[i].size();j++)
+        {
+            cout<<content[i][j]<<""<<endl;
+
+        }
+        cout<<"\n";
+
+
+    }
+
+    
+ 
+    return 0;
+}
+
+
+
